4d4bae63f3b1e75578455b72e9367bad7dd8ab55
Denisov Denis <www@ksk.by>
2023-09-12 10:39:50 +0300

    Рефакторинг класса Geography

--- src/lib/API/Service/Geography.php
+++ src/lib/API/Service/Geography.php
@@ -1,61 +1,68 @@
 <?php
 namespace Ipol\DPD\API\Service;
 
-use \Ipol\DPD\API\User\UserInterface;
-use \Ipol\DPD\API\Client\Factory as ClientFactory;
+use Exception;
+use Ipol\DPD\API\Client\ClientInterface;
+use Ipol\DPD\API\Client\Soap;
+use Ipol\DPD\API\User\UserInterface;
+use Ipol\DPD\API\Client\Factory as ClientFactory;
 
 /**
  * Служба по работе с географическими данными
  */
 class Geography implements ServiceInterface
 {
-	protected $wdsl = 'https://ws.dpd.ru/services/geography2?wsdl';
+	protected string $wdsl = 'https://ws.dpd.ru/services/geography2?wsdl';
 
 	protected $clientOld;
+    private Soap|ClientInterface $client;
 
-	/**
+    /**
      * Конструктор класса
-     * 
-     * @param \Ipol\DPD\API\User\UserInterface
+     *
+     * @param UserInterface $user
+     * @throws Exception
      */
 	public function __construct(UserInterface $user)
 	{
 		// По не известным причинам данный сервис в тестовом режиме
-		// выдает soap ошибку. В боевом - этой ошибки нет. 
+		// выдает soap ошибку. В боевом - этой ошибки нет.
 		// Поэтому для этого сервиса мы отключаем тестовый режим всегда
 		// $user = new User($user->getClientNumber(), $user->getSecretKey(), false);
 
 		$this->client = ClientFactory::create($this->wdsl, $user);
 	}
 
-	/**
-	 * Возвращает список городов с возможностью доставки наложенным платежом
-	 * 
-	 * @param string $countryCode код страны
-	 * 
-	 * @return array
-	 */
-	public function getCitiesCashPay($countryCode = 'RU')
-	{
+    /**
+     * Возвращает список городов с возможностью доставки наложенным платежом
+     *
+     * @param string $countryCode код страны
+     *
+     * @return array
+     * @throws Exception
+     */
+	public function getCitiesCashPay(string $countryCode = 'RU'): array
+    {
 		return $this->client->invoke('getCitiesCashPay', array(
 			'countryCode' => $countryCode
 		), 'request', 'cityCode');
 	}
 
-	/**
-	 * Возвращает список пунктов приема/выдачи посылок, имеющих ограничения по габаритам и весу, 
-	 * с указанием режима работы пункта и доступностью выполнения самопривоза/самовывоза.
-	 * При работе с  методом  необходимо проводить получение информации по списку подразделений ежедневно.
-	 * 
-	 * @param string $countryCode код страны
-	 * @param string $regionCode  код региона
-	 * @param string $cityCode    код города
-	 * @param string $cityName    название города
-	 * 
-	 * @return array
-	 */
-	public function getParcelShops($countryCode = 'RU', $regionCode = false, $cityCode = false, $cityName = false)
-	{
+    /**
+     * Возвращает список пунктов приема/выдачи посылок, имеющих ограничения по габаритам и весу,
+     * с указанием режима работы пункта и доступностью выполнения самопривоза/самовывоза.
+     * При работе с методом необходимо проводить получение информации по списку подразделений ежедневно.
+     *
+     * @param string $countryCode код страны
+     * @param bool|string $regionCode код региона
+     * @param bool|string $cityCode код города
+     * @param bool|string $cityName название города
+     *
+     * @return array
+     * @throws Exception
+     */
+	public function getParcelShops(string $countryCode = 'RU', bool|string $regionCode = false, bool|string $cityCode = false, bool|string $cityName = false): array
+    {
 		$ret = $this->client->invoke('getParcelShops', array_filter(array(
 			'countryCode' => $countryCode,
 			'regionCode'  => $regionCode,
@@ -70,13 +77,14 @@ class Geography implements ServiceInterface
 		return $ret;
 	}
 
-	/**
-	 * Возвращает список подразделений DPD, не имеющих ограничений по габаритам и весу посылок приема/выдачи
-	 *
-	 * @return array
-	 */
-	public function getTerminalsSelfDelivery2()
-	{
+    /**
+     * Возвращает список подразделений DPD, не имеющих ограничений по габаритам и весу посылок приема/выдачи
+     *
+     * @return array
+     * @throws Exception
+     */
+	public function getTerminalsSelfDelivery2(): array
+    {
 		$ret = $this->client->invoke('getTerminalsSelfDelivery2', array(), false);
 
 		if ($ret) {
@@ -86,18 +94,19 @@ class Geography implements ServiceInterface
 		return $ret;
 	}
 
-	/**
-	 * Возвращает информацию о сроке бесплатного хранения на пункте
-	 *
-	 * @param array $terminalCodes
-	 * @param array $serviceCode
-	 * 
-	 * @return array
-	 */
-	public function getStoragePeriod(array $terminalCodes = array(), array $serviceCode = array())
-	{
+    /**
+     * Возвращает информацию о сроке бесплатного хранения на пункте
+     *
+     * @param array $terminalCodes
+     * @param array $serviceCode
+     *
+     * @return array
+     * @throws Exception
+     */
+	public function getStoragePeriod(array $terminalCodes = array(), array $serviceCode = array()): array
+    {
 		return $this->client->invoke('getStoragePeriod', array_filter(array(
-			'terminalCоdes' => implode(',', $terminalCоdes),
+			'terminalCоdes' => implode(',', $terminalCodes),
 			'serviceCode'   => implode(',', $serviceCode)
 		)));
 	}
