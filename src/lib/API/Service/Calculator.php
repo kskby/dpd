57929ed072c5452096856f43256b54caadb9c0f9
Denisov Denis <www@ksk.by>
2023-11-13 13:14:53 +0300

    Обновлен класс Calculator

--- src/lib/API/Service/Calculator.php
+++ src/lib/API/Service/Calculator.php
@@ -1,59 +1,68 @@
 <?php
+
 namespace Ipol\DPD\API\Service;
 
-use \Ipol\DPD\API\User\UserInterface;
-use \Ipol\DPD\API\Client\Factory as ClientFactory;
+use Exception;
+use Ipol\DPD\API\Client\ClientInterface;
+use Ipol\DPD\API\Client\Soap;
+use Ipol\DPD\API\User\UserInterface;
+use Ipol\DPD\API\Client\Factory as ClientFactory;
 
 /**
  * Служба расчета стоимости доставки
  */
 class Calculator implements ServiceInterface
 {
-	protected $wdsl = 'https://ws.dpd.ru/services/calculator2?wsdl';
+    protected string $wdsl = 'https://ws.dpd.ru/services/calculator2?wsdl';
+    private Soap|ClientInterface $client;
 
-	/**
+    /**
      * Конструктор класса
-     * 
-     * @param \Ipol\DPD\API\User\UserInterface
+     *
+     * @param UserInterface $user
+     * @throws Exception
      */
-	public function __construct(UserInterface $user)
-	{
-		$this->client = ClientFactory::create($this->wdsl, $user);
-	}
+    public function __construct(UserInterface $user)
+    {
+        $this->client = ClientFactory::create($this->wdsl, $user);
+    }
 
-	/**
-	 * Рассчитать общую стоимость доставки по России и странам ТС.
-	 * 
-	 * @param  array  $parms
-	 * 
-	 * @return array
-	 */
-	public function getServiceCost(array $parms)
-	{
-		return $this->client->invoke('getServiceCost2', $parms, 'request', 'serviceCode');
-	}
+    /**
+     * Рассчитать общую стоимость доставки по России и странам ТС.
+     *
+     * @param array $parms
+     *
+     * @return array
+     * @throws Exception
+     */
+    public function getServiceCost(array $parms): array
+    {
+        return $this->client->invoke('getServiceCost2', $parms, 'request', 'serviceCode');
+    }
 
-	/**
-	 * Рассчитать стоимость доставки по параметрам  посылок по России и странам ТС.
-	 * 
-	 * @param  array  $parms
-	 * 
-	 * @return array
-	 */
-	public function getServiceCostByParcels(array $parms)
-	{
-		return $this->client->invoke('getServiceCostByParcels2', $parms, 'request', 'serviceCode');
-	}
+    /**
+     * Рассчитать стоимость доставки по параметрам  посылок по России и странам ТС.
+     *
+     * @param array $parms
+     *
+     * @return array
+     * @throws Exception
+     */
+    public function getServiceCostByParcels(array $parms): array
+    {
+        return $this->client->invoke('getServiceCostByParcels2', $parms, 'request', 'serviceCode');
+    }
 
-	/**
-	 * Рассчитать общую стоимость доставки по международным направлениям
-	 * 
-	 * @param  array  $parms
-	 * 
-	 * @return array
-	 */
-	public function getServiceCostInternational(array $parms)
-	{
-		return $this->client->invoke('getServiceCostInternational', $parms, 'request', 'serviceCode');
-	}
+    /**
+     * Рассчитать общую стоимость доставки по международным направлениям
+     *
+     * @param array $parms
+     *
+     * @return array
+     * @throws Exception
+     */
+    public function getServiceCostInternational(array $parms): array
+    {
+        return $this->client->invoke('getServiceCostInternational', $parms, 'request', 'serviceCode');
+    }
 }
